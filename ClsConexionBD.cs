db6276eddfba7da4e934e6a68ac9d153f0d16f84
Alejandro Zuniga <aj_zunigaz@unicah.edu>
2021-06-15 22:37:19 -0600

    cambios en la pantalla de Empleados y realizada la programacion de la pantalla

--- ClsConexionBD.cs
+++ ClsConexionBD.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
 
 namespace Pantallas_proyecto
 {
     public class ClsConexionBD
     {
         public SqlConnection conexion = new SqlConnection("Data Source = SQL5053.site4now.net; Initial Catalog = db_a75e9e_bderickmoncada; User Id = db_a75e9e_bderickmoncada_admin; Password = grp5admin");
+
+        SqlDataAdapter da;
+        DataTable dt;
+        DataSet DS;
+
         public void abrir()
         {
             try
@@ -21,6 +28,7 @@ namespace Pantallas_proyecto
                 Console.WriteLine("error al abrir BD " ,ex.Message);
             }
         }
+
         public void cerrar()
         {
             conexion.Close();
@@ -30,6 +38,26 @@ namespace Pantallas_proyecto
             return new SqlConnection("Data Source = SQL5053.site4now.net; Initial Catalog = db_a75e9e_bderickmoncada; User Id = db_a75e9e_bderickmoncada_admin; Password = grp5admin");
         }
 
+        public void cargarDatosEmpleados(DataGridView dgv)
+        {
+            try
+            {
+                da = new SqlDataAdapter("Select * From Empleados", conexion);
+                dt = new DataTable();
+                da.Fill(dt);
+                dgv.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pueden cargar los datos" + ex, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+
+
+     
     }
     
 }
--- FrmEmpleados.cs
+++ FrmEmpleados.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace Pantallas_proyecto
 {
@@ -16,6 +18,9 @@ namespace Pantallas_proyecto
         {
             InitializeComponent();
         }
+
+        ClsConexionBD conect = new ClsConexionBD();
+        SqlCommand cmd;
 
         private void label9_Click(object sender, EventArgs e)
         {
@@ -32,6 +37,51 @@ namespace Pantallas_proyecto
             FrmMenuCRUD cRUD = new FrmMenuCRUD();
             cRUD.Show();
             this.Close();
+        }
+
+        private void FrmEmpleados_Load(object sender, EventArgs e)
+        {
+            conect.abrir();
+            conect.cargarDatosEmpleados(dgvEmpleados);
+        }
+
+        
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtNombre.Text == "  " || txtApellido.Text == "  " || txtPuesto.Text == "  " || txtIdentidad.Text == "  " || txtNumeroTel.Text == "  ")
+                {
+                    MessageBox.Show("No se pueden Insertar datos en blanco", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    cmd = new SqlCommand("Insert into Empleados(codigo_puesto, nombre_empleado, apellido_empleado, numero_identidad_empleado, fecha_nacimiento, fecha_ingreso, num_telefono) Values(" + txtPuesto.Text + ",'" + txtNombre.Text + "', '" + txtApellido.Text + "', '" + txtIdentidad.Text + "', '" + dtpFechaNacimiento.Text + "','" + dtpFechaIngreso.Text + "','" + txtNumeroTel.Text + "')", conect.conexion);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Se han ingresado los Datos con Exito ", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conect.cargarDatosEmpleados(dgvEmpleados);
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToString());
+                MessageBox.Show("ERROR AL INSERTAR LOS DATOS", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                txtNombre.Clear();
+                txtApellido.Clear();
+                txtIdentidad.Clear();
+                txtPuesto.Clear();
+                txtNumeroTel.Clear();
+                txtNombre.Focus();
+            }
+
+        }
+
+        private void txtBuscarEmpleado_TextChanged(object sender, EventArgs e)
+        {
+            var aux = new MetodoBuscarEmpleado();
+            aux.filtrar(dgvEmpleados, this.txtBuscarEmpleado.Text.Trim());
         }
     }
 }
