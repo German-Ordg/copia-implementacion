67a7fbe7b0de1b2be4401c4788169ccaaaa39b36
calebandrade <vicaa_96@hotmail.com>
2021-06-17 00:34:28 -0600

    Agregar nuevo puesto y mostrarlo en DataGrid

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Pantallas_proyecto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmAcceso());
+            Application.Run(new frmPuestosTrabajo());//cambiar de nuevo a FrmAcceso()
         }
     }
 }
--- frmPuestosTrabajo.cs
+++ frmPuestosTrabajo.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Pantallas_proyecto
@@ -17,7 +12,23 @@ namespace Pantallas_proyecto
             InitializeComponent();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+      
+        SqlConnection conexion = new SqlConnection("Data Source = SQL5053.site4now.net; Initial Catalog = db_a75e9e_bderickmoncada; User Id = db_a75e9e_bderickmoncada_admin; Password = grp5admin");
+
+        private void frmPuestosTrabajo_Load(object sender, EventArgs e)
+        {
+            string consulta = "SELECT codigo_puesto as Código, descripcion_puesto as Puesto FROM Empleados_Puestos";
+            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, conexion);
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            Agregar();
+
+            DgvPuesto.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DgvPuesto.DataSource = tabla;
+
+        }
+
+        private void DgvPuesto_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
@@ -28,5 +39,17 @@ namespace Pantallas_proyecto
             cRUD.Show();
             cRUD.Hide();
         }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Empleados_Puestos (descripcion_puesto) VALUES (@puesto)";
+            conexion.Open();
+            SqlCommand comando = new SqlCommand(query, conexion);
+            comando.Parameters.AddWithValue("@puesto", txtPosicion.Text);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Nuevo Puesto Insertado");
+        }
+
+
     }
 }
