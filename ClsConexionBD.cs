5007747661d6f38b51f2f7372dee7e4fbc71c4e1
raulfer12 <85402765+raulfer12@users.noreply.github.com>
2021-06-24 11:00:17 -0600

    Update ClsConexionBD.cs

--- ClsConexionBD.cs
+++ ClsConexionBD.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
 
 namespace Pantallas_proyecto
 {
@@ -28,6 +30,28 @@ namespace Pantallas_proyecto
         protected SqlConnection GetSqlConnection()
         {
             return new SqlConnection("Data Source = SQL5053.site4now.net; Initial Catalog = db_a75e9e_bderickmoncada; User Id = db_a75e9e_bderickmoncada_admin; Password = grp5admin");
+        }
+        
+         SqlDataAdapter Da;
+        DataTable Dt;
+
+        public void CargarDatosUsuario(DataGridView dgv)
+        {
+            try
+            {
+                Da = new SqlDataAdapter("select * from Usuarios", conexion);
+                Dt = new DataTable();
+                Da.Fill(Dt);
+                dgv.DataSource = Dt;
+            }
+            catch (Exception ex)
+
+            {
+                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Error al cargar base de datos!", "ERROR...!"
+                                , MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
         }
 
     }
