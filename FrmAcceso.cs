e9cbda98e538d6135dbc8311e414eebca8b7acab
Leslie Ordoñez <lesliealvarez.or@gmail.com>
2021-06-24 12:14:43 -0600

    Cambios en el Form compras

--- FrmAcceso.cs
+++ FrmAcceso.cs
@@ -47,7 +47,7 @@ namespace Pantallas_proyecto
                     var validar = usuario.Login(txtUsuario.Text, txtContrasena.Text);
                     if (validar == true)
                     {                     
-                        FrmMenuPrincipalGerente menu = new FrmMenuPrincipalGerente();                      
+                        FrmMenuPrincipal menu = new FrmMenuPrincipal();                      
                         menu.Show();
                         menu.FormClosed += cerrarSesion;
                         this.Hide();
