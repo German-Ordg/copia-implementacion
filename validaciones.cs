62f835d8702bd39b4670de1cd4147483ac3bb928
Edgardo Rojas <erojas504@outlook.com>
2021-07-07 21:23:22 -0600

    Agregamos la validacion de espacios en blanco primera fase

--- validaciones.cs
+++ validaciones.cs
@@ -10,10 +10,35 @@ namespace Pantallas_proyecto
 {
     public partial class validaciones
     {
+        //Aqui validamos la negacion al encontrarse espacios en Blanco
+        public bool Espacio_Vacio(ErrorProvider ubicacionError, TextBox txt){
 
-        public void ValidarTextbox() {
-           
+            bool Espacio_Vacio = true;
+            foreach (char caracter in txt.Text)
+            {
+                if (!Char.IsWhiteSpace(caracter))
+                {
+                    Espacio_Vacio = false;
+                    ubicacionError.SetError(txt, "");
+                    break;
+                }
+                else {
+                    Espacio_Vacio = true;
+                }
+
+                if (caracter.ToString() = null)
+                {
+
+                }
+                else {
+                    ubicacionError.SetError();
+                }
+            }
+            return Espacio_Vacio;
+
         }
+       
+
 
     }
 }
