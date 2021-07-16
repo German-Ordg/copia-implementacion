d180c0430920d9411cec0cddb71664ee22779bbf
Jhosus <jhosus@outlook.es>
2021-07-16 14:27:45 -0600

    Validaciones de los campos pantalla Proveedores

--- frmProveedores.Designer.cs
+++ frmProveedores.Designer.cs
@@ -94,6 +94,8 @@ namespace Pantallas_proyecto
             this.txtNombreProovedor.Name = "txtNombreProovedor";
             this.txtNombreProovedor.Size = new System.Drawing.Size(289, 20);
             this.txtNombreProovedor.TabIndex = 49;
+            this.txtNombreProovedor.TextChanged += new System.EventHandler(this.txtNombreProovedor_TextChanged);
+            this.txtNombreProovedor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombreProovedor_KeyPress);
             // 
             // label3
             // 
@@ -154,6 +156,7 @@ namespace Pantallas_proyecto
             this.txtTelefono.Size = new System.Drawing.Size(289, 20);
             this.txtTelefono.TabIndex = 55;
             this.txtTelefono.TextChanged += new System.EventHandler(this.txtTelefono_TextChanged);
+            this.txtTelefono.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTelefono_KeyPress);
             // 
             // groupBox1
             // 
--- frmProveedores.cs
+++ frmProveedores.cs
@@ -178,7 +178,32 @@ namespace Pantallas_proyecto
 
         private void txtTelefono_TextChanged(object sender, EventArgs e)
         {
-           
+            
+        }
+
+        private void txtTelefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo Numeros", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtNombreProovedor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombreProovedor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo Letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
         }
     }
     }
