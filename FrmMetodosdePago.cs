079933bf865a0b49c216c2af359e97c1d05a1e40
Erick Moncada <esebastian2011@yahoo.es>
2021-07-15 23:37:42 -0600

    actualizar_8.2 con Rama_Sofia_4

--- FrmMetodosdePago.cs
+++ FrmMetodosdePago.cs
@@ -10,23 +10,12 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 
 namespace Pantallas_proyecto
-{    
+{
     public partial class FrmMetodosdePago : Form
     {
         public FrmMetodosdePago()
         {
             InitializeComponent();
-        }
-
-        private const int CP_NOCLOSE_BUTTON = 0x200;
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                CreateParams myCp = base.CreateParams;
-                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
-                return myCp;
-            }
         }
 
         ClsConexionBD conect = new ClsConexionBD();
@@ -49,13 +38,13 @@ namespace Pantallas_proyecto
         {
             try
             {
-                if(txtDescripcion.Text == "")
+                if (txtDescripcion.Text == "")
                 {
                     MessageBox.Show("No se pueden Insertar datos en blanco", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 else
                 {
-                    cmd = new SqlCommand("INSERT INTO Metodo_Pago (descripcion_pago) VALUES ('"+txtDescripcion.Text+"')", conect.conexion);
+                    cmd = new SqlCommand("INSERT INTO Metodo_Pago (descripcion_pago) VALUES ('" + txtDescripcion.Text + "')", conect.conexion);
                     cmd.ExecuteNonQuery();
                     MessageBox.Show("Los Datos han sido insertados con Exitos", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     conect.cargarMetodosPago(dgvMetodosPago);
@@ -86,7 +75,7 @@ namespace Pantallas_proyecto
                 }
                 else
                 {
-                    cmd = new SqlCommand("DELETE FROM Metodo_Pago Where codigo_pago = " + codigo , conect.conexion);
+                    cmd = new SqlCommand("DELETE FROM Metodo_Pago Where codigo_pago = " + codigo, conect.conexion);
                     cmd.ExecuteNonQuery();
                     MessageBox.Show("Se han eliminado los datos correctamente", "INFORMACIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     conect.cargarMetodosPago(dgvMetodosPago);
@@ -95,7 +84,7 @@ namespace Pantallas_proyecto
                     poc = -1;
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show("No se pudo eliminar los datos" + ex, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
@@ -107,10 +96,10 @@ namespace Pantallas_proyecto
         {
 
             poc = dgvMetodosPago.CurrentRow.Index;
-            
+
             try
             {
-                if(txtDescripcion.Text=="")
+                if (txtDescripcion.Text == "")
                 {
                     MessageBox.Show("Seleccione la descripción del método de Pago que desea modificar haciendo clic sobre la descripción que desea cambiar o modificar. Recuerde que Tampoco es permitido dejar sin descripción algún método de pago.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
@@ -118,8 +107,8 @@ namespace Pantallas_proyecto
                 {
                     codigo1 = Convert.ToInt32(dgvMetodosPago[0, poc].Value);
                     dgvMetodosPago[1, poc].Value = txtDescripcion.Text;
-                      
-                    cmd = new SqlCommand("UPDATE Metodo_Pago SET descripcion_pago = '" + txtDescripcion.Text +"' WHERE codigo_pago = "+codigo1, conect.conexion);
+
+                    cmd = new SqlCommand("UPDATE Metodo_Pago SET descripcion_pago = '" + txtDescripcion.Text + "' WHERE codigo_pago = " + codigo1, conect.conexion);
                     cmd.ExecuteNonQuery();
                     MessageBox.Show("El Registro fue actualizado exitosamente.", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     conect.cargarMetodosPago(dgvMetodosPago);
@@ -127,7 +116,7 @@ namespace Pantallas_proyecto
                     txtDescripcion.Clear();
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show("No se pudo modificar los datos" + ex, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
@@ -158,6 +147,13 @@ namespace Pantallas_proyecto
         private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
 
+        }
+
+        private void dgvMetodosPago_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            poc = dgvMetodosPago.CurrentRow.Index;
+            codigo = dgvMetodosPago[0, poc].Value.ToString();
+            txtDescripcion.Text = dgvMetodosPago[1, poc].Value.ToString();
         }
     }
 }
