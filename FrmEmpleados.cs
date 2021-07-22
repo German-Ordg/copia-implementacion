61ae6c6c33fd7f40711bf249a162ee6a62432ef3
Alejandro  Josue Zuniga <aj_zunigaz@unicah.edu>
2021-07-22 13:46:46 -0600

    validaciones en el boton de modificar

--- FrmEmpleados.cs
+++ FrmEmpleados.cs
@@ -17,14 +17,10 @@ namespace Pantallas_proyecto
 
         private bool letra1 = false;
         private bool letra2 = false;
-        private bool letra3 = false;
-        private bool letra4 = false;
-        private bool letra5 = false;
         private bool numero1 = false;
         private bool numero2 = false;
-        private bool numero3 = false;
-        private bool numero4 = false;
-        private bool numero5 = false;
+        public int EmpleadoEdad;
+        
         public FrmEmpleados()
         {
             InitializeComponent();
@@ -78,8 +74,128 @@ namespace Pantallas_proyecto
         private void btnAgregar_Click(object sender, EventArgs e)
         {
 
-            letra1 = false; letra2 = false; letra3 = false; letra4 = false; letra5 = false;
-            numero1 = false; numero2 = false; numero3 = false; numero4 = false;
+            letra1 = false; letra2 = false; numero1 = false; numero2 = false; 
+
+            if (validacion.Espacio_Blanco(ErrorProvider1, txtNombre) || validacion.Solo_Letras(ErrorProvider1, txtNombre))
+            {
+                if (validacion.Espacio_Blanco(ErrorProvider1, txtNombre))
+                    ErrorProvider1.SetError(txtNombre, "no se puede dejar en blanco");
+                else
+                if (validacion.Solo_Letras(ErrorProvider1, txtNombre))
+                    ErrorProvider1.SetError(txtNombre, "Solo se permite letras");
+            }
+            else
+            {
+                letra1 = true;
+            }
+
+            if (validacion.Espacio_Blanco(ErrorProvider1, txtApellido) || validacion.Solo_Letras(ErrorProvider1, txtApellido))
+            {
+                if (validacion.Espacio_Blanco(ErrorProvider1, txtApellido))
+                    ErrorProvider1.SetError(txtApellido, "no se puede dejar en blanco");
+                else
+                if (validacion.Solo_Letras(ErrorProvider1, txtApellido))
+                    ErrorProvider1.SetError(txtApellido, "Solo se permite letras");
+            }
+            else
+            {
+                letra2 = true;
+            }
+
+            if (validacion.Espacio_Blanco(ErrorProvider1, txtIdentidad) || validacion.Solo_Numeros(ErrorProvider1, txtIdentidad))
+            {
+                if (validacion.Espacio_Blanco(ErrorProvider1, txtIdentidad))
+                    ErrorProvider1.SetError(txtIdentidad, "no se puede dejar en blanco");
+                else
+                if (validacion.Solo_Numeros(ErrorProvider1, txtIdentidad))
+                    ErrorProvider1.SetError(txtIdentidad, "Solo se permite numeros");
+            }
+            else
+            {
+                if (txtIdentidad.Text.Length != 13)
+                {
+                    MessageBox.Show("Ingrese 13 digitos en su identidad", "Falta de datos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtNombre.Focus();
+                }
+                else
+                {
+                    numero1 = true;
+                }
+
+
+                if (validacion.Espacio_Blanco(ErrorProvider1, txtNumeroTel) || validacion.Solo_Numeros(ErrorProvider1, txtNumeroTel))
+                {
+                    if (validacion.Espacio_Blanco(ErrorProvider1, txtNumeroTel))
+                        ErrorProvider1.SetError(txtNumeroTel, "No se puede dejar en blanco");
+                    else
+                    if (validacion.Solo_Numeros(ErrorProvider1, txtNumeroTel))
+                        ErrorProvider1.SetError(txtNumeroTel, "Solo se permite numeros");
+                }
+
+                if (txtNumeroTel.Text.Length != 8)
+                {
+                    MessageBox.Show("Ingrese 8 digitos en el telefono del empleado", "Falta de datos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtNumeroTel.Focus();
+
+                }
+                else
+                {
+                    numero2 = true;
+                }
+
+                if (numero1 && letra1 && letra2 && numero2)
+                {
+
+                    
+
+                    try
+                    {
+
+                        cmd = new SqlCommand("Insert into Empleados(codigo_puesto, nombre_empleado, apellido_empleado, numero_identidad_empleado, fecha_nacimiento, fecha_ingreso, num_telefono, Genero) Values(" + txtPuesto.Text + ",'" + txtNombre.Text + "', '" + txtApellido.Text + "', '" + txtIdentidad.Text + "', '" + dtpFechaNacimiento.Text + "','" + dtpFechaIngreso.Text + "','" + txtNumeroTel.Text + "','" + cmbGenero.Text + "')", conect.conexion);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Se han ingresado los Datos con Exito ", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        conect.cargarDatosEmpleados(dgvEmpleados);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.Message.ToString());
+                        MessageBox.Show("ERROR AL INSERTAR LOS DATOS", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        txtNombre.Text = " ";
+                        txtApellido.Text = " ";
+                        txtIdentidad.Text = " ";
+                        txtPuesto.Text = " ";
+                        txtNumeroTel.Text = " ";
+                        dtpFechaNacimiento.Text = DateTime.Now.ToShortDateString();
+                        dtpFechaIngreso.Text = DateTime.Now.ToShortDateString();
+                        cmbGenero.SelectedIndex = -1;
+                        txtNombre.Focus();
+                    }
+                }
+
+           
+            }
+
+
+
+
+
+        }
+
+        private void txtBuscarEmpleado_TextChanged(object sender, EventArgs e)
+        {
+            var aux = new MetodoBuscarEmpleado();
+            aux.filtrar(dgvEmpleados, this.txtBuscarEmpleado.Text.Trim());
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            int indice;
+            int codigo;
+            indice = dgvEmpleados.CurrentRow.Index;
+
+            letra1 = false; letra2 = false; numero1 = false; numero2 = false;
 
             if (validacion.Espacio_Blanco(ErrorProvider1, txtNombre) || validacion.Solo_Letras(ErrorProvider1, txtNombre))
             {
@@ -153,17 +269,22 @@ namespace Pantallas_proyecto
 
                     try
                     {
+                        codigo = Convert.ToInt32(dgvEmpleados[0, indice].Value);
 
-                        cmd = new SqlCommand("Insert into Empleados(codigo_puesto, nombre_empleado, apellido_empleado, numero_identidad_empleado, fecha_nacimiento, fecha_ingreso, num_telefono, Genero) Values(" + txtPuesto.Text + ",'" + txtNombre.Text + "', '" + txtApellido.Text + "', '" + txtIdentidad.Text + "', '" + dtpFechaNacimiento.Text + "','" + dtpFechaIngreso.Text + "','" + txtNumeroTel.Text + "','" + cmbGenero.Text + "')", conect.conexion);
+
+                        dgvEmpleados[1, indice].Value = txtPuesto.Text;
+                        dgvEmpleados[2, indice].Value = txtNombre.Text;
+                        dgvEmpleados[3, indice].Value = txtApellido.Text;
+                        dgvEmpleados[4, indice].Value = txtIdentidad.Text;
+                        dgvEmpleados[5, indice].Value = dtpFechaNacimiento.Text;
+                        dgvEmpleados[6, indice].Value = dtpFechaIngreso.Text;
+                        dgvEmpleados[7, indice].Value = txtNumeroTel.Text;
+                        dgvEmpleados[8, indice].Value = cmbGenero.Text;
+
+                        cmd = new SqlCommand("update Empleados set codigo_puesto = " + txtPuesto.Text + ", nombre_empleado ='" + txtNombre.Text + "', apellido_empleado = '" + txtApellido.Text + "', numero_identidad_empleado= '" + txtIdentidad.Text + "', fecha_nacimiento = '" + dtpFechaNacimiento.Text + "', fecha_ingreso= '" + dtpFechaIngreso.Text + "', num_telefono = '" + txtNumeroTel.Text + "',Genero='" + cmbGenero.Text + "'  where codigo_empelado = " + codigo, conect.conexion);
                         cmd.ExecuteNonQuery();
-                        MessageBox.Show("Se han ingresado los Datos con Exito ", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        MessageBox.Show("El registro fue actualizado exitosamente");
                         conect.cargarDatosEmpleados(dgvEmpleados);
-
-                    }
-                    catch (Exception ex)
-                    {
-                        //MessageBox.Show(ex.Message.ToString());
-                        MessageBox.Show("ERROR AL INSERTAR LOS DATOS", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
                         txtNombre.Text = " ";
                         txtApellido.Text = " ";
@@ -174,63 +295,14 @@ namespace Pantallas_proyecto
                         dtpFechaIngreso.Text = DateTime.Now.ToShortDateString();
                         cmbGenero.SelectedIndex = -1;
                         txtNombre.Focus();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("El registro no pudo ser actualizado" + ex);
                     }
                 }
 
-           
-            }
-
-
-
-
-
-        }
-
-        private void txtBuscarEmpleado_TextChanged(object sender, EventArgs e)
-        {
-            var aux = new MetodoBuscarEmpleado();
-            aux.filtrar(dgvEmpleados, this.txtBuscarEmpleado.Text.Trim());
-        }
-
-        private void btnModificar_Click(object sender, EventArgs e)
-        {
-            int indice;
-            int codigo;
-            indice = dgvEmpleados.CurrentRow.Index;
-
-            try
-            {
-                codigo = Convert.ToInt32(dgvEmpleados[0, indice].Value);
-
-              
-                dgvEmpleados[1, indice].Value = txtPuesto.Text;
-                dgvEmpleados[2, indice].Value = txtNombre.Text;
-                dgvEmpleados[3, indice].Value = txtApellido.Text;
-                dgvEmpleados[4, indice].Value = txtIdentidad.Text;
-                dgvEmpleados[5, indice].Value = dtpFechaNacimiento.Text;
-                dgvEmpleados[6, indice].Value = dtpFechaIngreso.Text;
-                dgvEmpleados[7, indice].Value = txtNumeroTel.Text;
-                dgvEmpleados[8, indice].Value = cmbGenero.Text;
-
-                cmd = new SqlCommand("update Empleados set codigo_puesto = " +txtPuesto.Text+ ", nombre_empleado ='"+txtNombre.Text+"', apellido_empleado = '"+txtApellido.Text+"', numero_identidad_empleado= '"+txtIdentidad.Text+"', fecha_nacimiento = '"+dtpFechaNacimiento.Text+"', fecha_ingreso= '"+dtpFechaIngreso.Text+"', num_telefono = '"+txtNumeroTel.Text+ "',Genero='" + cmbGenero.Text+ "'  where codigo_empelado = " + codigo, conect.conexion);
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("El registro fue actualizado exitosamente");
-                conect.cargarDatosEmpleados(dgvEmpleados);
-
-                txtNombre.Text = " ";
-                txtApellido.Text = " ";
-                txtIdentidad.Text= " ";
-                txtPuesto.Text= " ";
-                txtNumeroTel.Text= " ";
-                dtpFechaNacimiento.Text = DateTime.Now.ToShortDateString();
-                dtpFechaIngreso.Text = DateTime.Now.ToShortDateString();
-                cmbGenero.SelectedIndex = -1;
-                txtNombre.Focus();
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("El registro no pudo ser actualizado" + ex);
             }
         }
 
