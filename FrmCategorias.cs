56326a8625a6b4a33939cec0d7a9ca406fe94601
Edgardo Rojas <erojas504@outlook.com>
2021-07-21 23:41:16 -0600

    Avance 1 en la programacion del boton agregar

--- FrmCategorias.cs
+++ FrmCategorias.cs
@@ -41,5 +41,18 @@ namespace Pantallas_proyecto
             menuCRUD.Show();
             this.Close();
         }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            try {
+                if (){
+                
+                }
+                else {
+                
+                }
+            }
+            catch { }
+        }
     }
 }
